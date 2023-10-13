42e25b5ab940f889b6b71079619bc29e7171efd8
가인철 <45278265+entellaKa@users.noreply.github.com>
2023-10-14 02:49:14 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -3,9 +3,9 @@ k = int(input())
 for z in range(k):
     n, m = map(int, input().split(" "))
 
-    x = 1
+    k = 1
     for i in range(m,n,-1):
-        x*=i
+        k*=i
     for i in range(1, m-n+1):
-        x//=i
-    print(x)
+        k//=i
+    print(k)
